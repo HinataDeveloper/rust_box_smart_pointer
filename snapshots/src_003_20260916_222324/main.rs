8702e57dc6a7104f9ac088928010b401ad6806dd
Hinata <StubbornStruggle@gmail.com>
2026-09-16 22:24:01 +0330

    Treating smart pointer like regular reference

---
+++ snapshots/src_003_20260916_222324/main.rs
@@ -0,0 +1,46 @@
+// Date: Thu Sep 16 2026
+
+// Project: Learning Chapter 15
+// Goal: Using Smart Pointer: Treating smart pointer like regular reference
+// Dependency: Without dependency
+
+// rustc 1.100.0-nightly (215a8af4b 2026-09-15)
+// binary: rustc
+// commit-hash: 215a8af4bb4c106cccf6d6535f84eaae91818265
+// commit-date: 2026-09-15
+// host: x86_64-unknown-linux-gnu
+// release: 1.100.0-nightly
+// LLVM version: 23.1.1
+
+// cargo 1.100.0-nightly (7941be6fb 2026-09-11)
+// release: 1.100.0-nightly
+// commit-hash: 7941be6fb416b4cd9666aef7b858dfea25587a8c
+// commit-date: 2026-09-11
+// host: x86_64-unknown-linux-gnu
+// libgit2: 1.9.6 (sys:0.21.0 vendored)
+// libcurl: 8.21.0-DEV (sys:0.4.90+curl-8.21.0 vendored ssl:OpenSSL/3.6.3)
+// ssl: OpenSSL 3.6.3 9 Jun 2026
+// os: Fedora 44.0.0 [64-bit]
+
+// Kernel Version: 7.2.5-200.fc44.x86_64
+// Firmware Version: 71CN51WW(V1.21)
+
+fn main() {
+    println!("\n");
+
+    let x = 391;
+    let y = &x;
+
+    assert_eq!(391, x);
+
+    // Encounter with compile time error.
+    // Because y variable should be dreference.
+    // assert_eq!(391, y);
+
+    assert_eq!(391, *y);
+
+    println!("value of x is: {}", x);
+    println!("value of y is: {}", *y);
+
+    println!("\nThe End ...\n");
+}
