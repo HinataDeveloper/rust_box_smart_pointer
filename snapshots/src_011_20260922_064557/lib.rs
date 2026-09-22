4b77d25031718369a984f07574fc121d1f84a563
Hinata <StubbornStruggle@gmail.com>
2026-09-22 06:51:08 +0330

    implementing Drop trait

---
+++ snapshots/src_011_20260922_064557/lib.rs
@@ -0,0 +1,83 @@
+// lib.rs
+// Date: Thu Sep 22 2026
+// Educational and Practice Rust Programming Language Code
+
+// rustc 1.100.0-nightly (bba531001 2026-09-20)
+// binary: rustc
+// commit-hash: bba531001d4de6d7f49693e0836a2668ca063282
+// commit-date: 2026-09-20
+// host: x86_64-unknown-linux-gnu
+// release: 1.100.0-nightly
+// LLVM version: 23.1.1
+//
+// cargo 1.100.0-nightly (495c385d0 2026-09-16)
+// release: 1.100.0-nightly
+// commit-hash: 495c385d0875c4ba51eb72ea0448a2d4c018b8d4
+// commit-date: 2026-09-16
+// host: x86_64-unknown-linux-gnu
+// libgit2: 1.9.6 (sys:0.21.0 vendored)
+// libcurl: 8.21.0-DEV (sys:0.4.90+curl-8.21.0 vendored ssl:OpenSSL/3.6.3)
+// ssl: OpenSSL 3.6.3 9 Jun 2026
+// os: Fedora 44.0.0 [64-bit]
+
+// Kernel Version: 7.2.5-200.fc44.x86_64
+// Firmware Version: 71CN51WW(V1.21)
+
+use std::fmt::Display;
+
+pub struct User {
+    user_id: u64,
+    username: String,
+    password: String,
+}
+
+impl User {
+    pub fn new(user_id: u64, username: impl Into<String>, password: impl Into<String>) -> User {
+        User {
+            user_id,
+            username: username.into(),
+            password: password.into(),
+        }
+    }
+
+    pub fn set_user_id(&mut self, user_id: u64) {
+        self.user_id = user_id;
+    }
+
+    pub fn set_username(&mut self, username: impl Into<String>) {
+        self.username = username.into();
+    }
+
+    pub fn set_password(&mut self, password: impl Into<String>) {
+        self.password = password.into();
+    }
+
+    pub fn user_id(&self) -> u64 {
+        self.user_id
+    }
+
+    pub fn username(&self) -> &str {
+        &self.username
+    }
+
+    pub fn password(&self) -> &str {
+        "******"
+    }
+}
+
+impl Drop for User {
+    fn drop(&mut self) {
+        // This is a symbolic code line
+        println!(" -> Goodbye {}", self.username);
+    }
+}
+
+impl Display for User {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "[user_id: {}, username: {}, password: ******]",
+            self.user_id, self.username
+        )
+    }
+}
---
+++ snapshots/src_011_20260922_064557/main.rs
@@ -0,0 +1,39 @@
+// main.rs
+// Date: Thu Sep 22 2026
+// Educational and Practice Rust Programming Language Code
+
+// Project: Learning Chapter 15
+// Goal: ...
+// Dependency: Without dependency
+
+// rustc 1.100.0-nightly (bba531001 2026-09-20)
+// binary: rustc
+// commit-hash: bba531001d4de6d7f49693e0836a2668ca063282
+// commit-date: 2026-09-20
+// host: x86_64-unknown-linux-gnu
+// release: 1.100.0-nightly
+// LLVM version: 23.1.1
+
+// cargo 1.100.0-nightly (495c385d0 2026-09-16)
+// release: 1.100.0-nightly
+// commit-hash: 495c385d0875c4ba51eb72ea0448a2d4c018b8d4
+// commit-date: 2026-09-16
+// host: x86_64-unknown-linux-gnu
+// libgit2: 1.9.6 (sys:0.21.0 vendored)
+// libcurl: 8.21.0-DEV (sys:0.4.90+curl-8.21.0 vendored ssl:OpenSSL/3.6.3)
+// ssl: OpenSSL 3.6.3 9 Jun 2026
+// os: Fedora 44.0.0 [64-bit]
+
+// Kernel Version: 7.2.5-200.fc44.x86_64
+// Firmware Version: 71CN51WW(V1.21)
+
+use box_smart_pointer::User;
+
+fn main() {
+    println!("\n");
+
+    let raphael: User = User::new(1003, "@Raphael", "321654987");
+    println!("raphael: {}", raphael);
+
+    println!("\nThe End ...\n");
+}
